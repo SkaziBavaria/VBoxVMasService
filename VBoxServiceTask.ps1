7e805dc8e05ebd723a3e99a2aca0f00ece7e22dc
SkaziBavaria <50840634+SkaziBavaria@users.noreply.github.com>
2023-03-29 09:34:15 +0200

    Update VBoxServiceTask.ps1

--- VBoxServiceTask.ps1
+++ VBoxServiceTask.ps1
@@ -1,67 +1,103 @@
-[CmdletBinding()]
+# This script creates a scheduled task for starting a VirtualBox VM according to the specified Frequency.
+
 param (
-    [Parameter(Mandatory=$false)]
-    [string]$TaskName = "$VMName as a service",
-    [Parameter(Mandatory=$false)]
-    [string]$TaskDescription = "Vbox VM $VMName as a service at startup", 
+    # Name of the VirtualBox VM to start.
     [Parameter(Mandatory=$true)]
     [string]$VMName,
-    [Parameter(Mandatory=$false)]
-    [string]$TaskPath = "My Custom Tasks",
-    [Parameter(Mandatory=$false)]
-    [string]$VBoxManagePath = "C:\PROGRA~1\Oracle\VirtualBox\VBoxManage.exe",
-    [Parameter(Mandatory=$false)]
-    [ValidateSet("AtStartup", "Once", "Daily", "Weekly", "Monthly", "OnIdle")]
-    [string]$Trigger = "AtStartup",
-    [Parameter(Mandatory=$false)]
-    [string]$User = "NT AUTHORITY\SYSTEM",
-    [Parameter(Mandatory=$false)]
+
+    # Frequency frequency for the scheduled task.
+    # Possible values: Once, Daily, Weekly, Monthly, AtLogOn, AtStartup
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Once", "Daily", "Weekly", "Monthly", "AtLogOn", "AtStartup")]
+    [string]$Frequency = "AtStartup",
+
+    # Name of the scheduled task.
+    [Parameter(Mandatory=$true)]
+    [string]$TaskName = "Start-VM-$VMName",
+
+    # Run level for the scheduled task.
+    # Possible values: Highest, Limited
     [ValidateSet("Highest", "Limited")]
     [string]$RunLevel = "Highest",
-    [Parameter(Mandatory=$false)]
-    [DateTime]$StartTime = (Get-Date),
-    [Parameter(Mandatory=$false)]
-    [DateTime]$StartDate = (Get-Date),
-    [Parameter(Mandatory=$false)]
-    [string]$DaysofWeek = "Monday,Wednesday,Friday",
-    [Parameter(Mandatory=$false)]
-    [string]$DaysofMonth = "1,15",
-    [Parameter(Mandatory=$false)]
-    [string]$Weeksinterval = "1",
-    [Parameter(Mandatory=$false)]
-    [string]$Monthsinterval = "1"   
 
+    # Path for the scheduled task.
+    [string]$TaskPath = "My Custom Tasks",
+
+    # Description for the scheduled task.
+    [string]$TaskDescription = "Vbox VM $VMName as a service at startup",
+
+    # Path to the VBoxManage executable.
+    [string]$VBoxManagePath = "C:\PROGRA~1\Oracle\VirtualBox\VBoxManage.exe",
+
+    # Start date for the scheduled task (format: yyyy-MM-dd).
+    [string]$StartDate,
+
+    # Start time for the scheduled task (format: HH:mm).
+    [string]$StartTime,
+
+    # Interval in days for the daily Frequency.
+    [UInt32]$DaysInterval,
+
+    # Days of the week for the weekly Frequency.
+    # Possible values: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    [DayOfWeek[]]$DaysOfWeek,
+
+    # Interval in weeks for the weekly Frequency.
+    [UInt32]$WeeksInterval,
+
+    # Days of the month for the monthly Frequency (1-31).
+    [UInt32[]]$DaysOfMonth,
+
+    # Months of the year for the monthly Frequency (1-12).
+    [UInt32[]]$MonthsOfYear,
+
+    # User account to run the scheduled task.
+    [string]$User = "NT AUTHORITY\SYSTEM"
 )
 
-# Create task
+# Combine the start date and time into a single DateTime object.
+$combinedDateTime = [DateTime]::ParseExact("$StartDate $StartTime", "yyyy-MM-dd HH:mm", $null)
+
+# Create a new scheduled task action to start the VM.
 $action = New-ScheduledTaskAction -Execute $VBoxManagePath -Argument "startvm $VMName --type headless"
 
-switch ($Trigger) {
-    "AtStartup" {
-        $trigger = New-ScheduledTaskTrigger -AtStartup
-    }
+# Set the Frequency based on the specified frequency.
+switch ($Frequency) {
     "Once" {
-        $dateTime = $StartDate.AddHours($StartTime.Hour).AddMinutes($StartTime.Minute)
-        $trigger = New-ScheduledTaskTrigger -Once $dateTime
+        $Frequency = New-ScheduledTaskFrequency -Once -At $combinedDateTime
     }
     "Daily" {
-        $trigger = New-ScheduledTaskTrigger -Daily -At $StartTime
+        if ($DaysInterval -gt 0) {
+            $Frequency = New-ScheduledTaskFrequency -Daily -At $combinedDateTime -DaysInterval $DaysInterval
+        } else {
+            $Frequency = New-ScheduledTaskFrequency -Daily -At $combinedDateTime
+        }
     }
     "Weekly" {
-        $trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $DaysofWeek -At $StartTime -WeeksInterval $Weeksinterval -StartDate $StartDate
+        $Frequency = New-ScheduledTaskFrequency -Weekly -At $combinedDateTime -DaysOfWeek $DaysOfWeek -WeeksInterval $WeeksInterval
     }
     "Monthly" {
-        $trigger = New-ScheduledTaskTrigger -Monthly -DaysOfMonth $DaysofMonth -At $StartTime -MonthsInterval $Monthsinterval -StartDate $StartDate
+        $Frequency = New-ScheduledTaskFrequency -Once -At $combinedDateTime
+        $Frequency.DaysOfMonth = $DaysOfMonth
+        $Frequency.MonthsOfYear = $MonthsOfYear
     }
-    "OnIdle" {
-        $trigger = New-ScheduledTaskTrigger -OnIdle -RandomDelay (New-TimeSpan -Minutes 15)
+    "AtLogOn" {
+        $Frequency = New-ScheduledTaskFrequency -AtLogOn -User $User
+    }
+    "AtStartup" {
+        $Frequency = New-ScheduledTaskFrequency -AtStartup
     }
     default {
-        Write-Error "Invalid value for Trigger parameter: $Trigger"
-        return
+        Write-Host "Invalid frequency specified. Exiting script."
+        exit
     }
 }
 
-$principal = New-ScheduledTaskPrincipal -UserId $User -RunLevel $RunLevel
-$setting = New-ScheduledTaskSettingsSet -RestartInterval (New-TimeSpan -Minutes 5) -RestartCount 3 -ExecutionTimeLimit 365 -AllowStartIfOnBatteries
-Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $setting -TaskPath $TaskPath -TaskName $TaskName -Description $TaskDescription
+# Define the settings for the scheduled task.
+$settings = New-ScheduledTaskSettingsSet -RestartInterval (New-TimeSpan -Minutes 5) -RestartCount 3 -ExecutionTimeLimit 365 -AllowStartIfOnBatteries
+
+# Define the principal (user account and run level) for the scheduled task.
+$principal = New-ScheduledTaskPrincipal -UserId $User -LogonType ServiceAccount -RunLevel $RunLevel
+
+# Register (create) the scheduled task with the provided parameters.
+Register-ScheduledTask -Action $action -Trigger $Frequency -Principal $principal -Settings $settings -TaskPath $TaskPath -TaskName $TaskName -Description $TaskDescription
